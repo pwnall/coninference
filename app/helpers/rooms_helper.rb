b04a1362b2032d142751f571f34c0333466a4edd
Staphany Park <stapark008@gmail.com>
2015-08-21 13:59:00 -0700

    Add occupancy indicator to rooms page.

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -3,4 +3,12 @@ module RoomsHelper
   def device_room_name(device)
     device.room ? device.room.name : 'Unassigned'
   end
+
+  def room_occupancy_indicator(room)
+    occupancy = room.occupied? ? 'busy' : 'free'
+    classes = ['occupancy-indicator', occupancy].join ' '
+    content_tag :div, class: classes do
+      occupancy.capitalize
+    end
+  end
 end
